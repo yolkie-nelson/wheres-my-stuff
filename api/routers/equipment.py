431aa7c300a29f98bd119cb199f92f94fbb27f31
Peace Omoregie <peaceomoregie1@gmail.com>
2024-01-22 17:15:32 -0500

    Equipment get and create api complete

--- api/routers/equipment.py
+++ api/routers/equipment.py
@@ -6,33 +6,25 @@ from fastapi import (
     HTTPException,
     status
 )
-from queries.equipment import EquipmentIn, EquipmentQueries, EquipmentOut
+from queries.equipment import EquipmentIn, EquipmentQueries, EquipmentOut, Error
+from queries.storage_site import StorageSiteQueries
+from queries.equipment_type import EquipmentTypeQueries
 from authenticator import authenticator
-from typing import Optional
+from typing import Union, List
 
 
 router = APIRouter()
 
-@router.post("/api/equipment/", response_model=EquipmentOut)
-async def create_equipment(
-    info: EquipmentIn,
-    # request: Request,
-    # response: Response,
+@router.get("/api/equipments/", response_model=Union[List[EquipmentOut], Error])
+def get_equipment(
     queries: EquipmentQueries = Depends()
 ):
-    try:
-        create_equipment = queries.create_equipment(info=info)
-    except:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Cannot create an equipment type"
-        )
-    form = EquipmentIn(equipment_type=info.equipment_type,
-                        model_name=info.model_name,
-                        description=info.description,
-                        serial_number=info.serial_number,
-                        warehouse=info.warehouse,
-                        date_serviced=info.date_serviced,
-                        photo=info.photo)
-    # token = await authenticator.login(response, request, form, repo)
-    return EquipmentOut(form)
+    return queries.get_equipment()
+
+@router.post("/api/equipments/", response_model=Union[EquipmentOut, Error])
+def create_equipment(
+    equipment: EquipmentIn,
+    queries: EquipmentQueries = Depends(),
+):
+
+    return queries.create_equipment(equipment)
