e1b948703669772f0e4a7c8379bd934eed88c63b
Peace Omoregie <peaceomoregie1@gmail.com>
2024-01-25 17:08:01 -0500

    created navbar

---
+++ ghi/src/Nav.jsx
@@ -0,0 +1,114 @@
+import { Link, Outlet} from "react-router-dom";
+
+
+function Nav() {
+    return (
+        <nav className="bg-gray-800">
+            <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                <div className="relative flex h-16 items-center justify-between">
+                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+
+                    <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+                    <span className="absolute -inset-0.5"></span>
+                    <span className="sr-only">Open main menu</span>
+
+                    <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    </svg>
+
+                    <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                    </button>
+                </div>
+                <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                    <div className="flex flex-shrink-0 items-center">
+                    <img className="h-10 w-auto" src="./public/wheres_my_stuff-removebg-preview.png" alt="Where's My Stuff Logo" />
+                    </div>
+                    <div className="text-white pr-10 pl-5">
+                        Where's My Stuff?
+                    </div>
+                    <div className="hidden sm:ml-6 sm:block">
+                    <div className="flex space-x-4">
+
+                        <a href="#" className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
+                        <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About</a>
+                        <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Login</a>
+                    </div>
+                    </div>
+                </div>
+                <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+
+
+
+                    <div className="relative ml-3">
+                    <div>
+                        <button type="button" className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                        <span className="absolute -inset-1.5"></span>
+                        <span className="sr-only">Open user menu</span>
+                        <img className="h-8 w-8 rounded-full"  />
+                        </button>
+                    </div>
+
+                    {/* <!--
+                        Dropdown menu, show/hide based on menu state.
+
+                        Entering: "transition ease-out duration-100"
+                        From: "transform opacity-0 scale-95"
+                        To: "transform opacity-100 scale-100"
+                        Leaving: "transition ease-in duration-75"
+                        From: "transform opacity-100 scale-100"
+                        To: "transform opacity-0 scale-95"
+                    --> */}
+                    {/* <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex="-1">
+                        <!-- Active: "bg-gray-100", Not Active: "" -->
+                        <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-0">Your Profile</a>
+                        <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-1">Settings</a>
+                        <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-2">Sign out</a>
+                    </div> */}
+                    </div>
+                </div>
+                </div>
+            </div>
+
+            {/* <!-- Mobile menu, show/hide based on menu state. --> */}
+            <div className="sm:hidden" id="mobile-menu">
+                <div className="space-y-1 px-2 pb-3 pt-2">
+                {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
+                <a href="#" className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Dashboard</a>
+                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Team</a>
+                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Projects</a>
+                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Calendar</a>
+                </div>
+            </div>
+            </nav>
+
+
+        // <nav classNameName="Navbar">
+        //     <h1 classNameName="text-3xl font-bold underline">
+        //         Hello world!
+        //     </h1>
+        //     <div>
+        //         <img classNameName="logo" src={"./public/wheres_my_stuff-removebg-preview.png"} />
+        //     </div>
+        //     <div>
+        //         <div>
+        //             Where's My Stuff
+        //         </div>
+        //         <div>
+        //             Tag line tag line tag line
+        //         </div>
+        //     </div>
+        //     <li>
+        //         <ul> </ul>
+        //         <ul>Home</ul>
+        //         <ul>About</ul>
+
+        //     </li>
+
+        // </nav>
+
+    );
+}
+
+export default Nav;
