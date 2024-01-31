922099599fda2d3ef60fb48472a019c34a694148
yolkie-nelson <144482873+yolkie-nelson@users.noreply.github.com>
2024-01-31 16:23:39 -0500

    working equipment list page

--- ghi/src/EquipmentList.jsx
+++ ghi/src/EquipmentList.jsx
@@ -1,15 +1,28 @@
 import { useState } from 'react';
-import { useGetEquipmentQuery } from "./app/apiSlice.js";
+import { useGetEquipmentQuery, useGetEquipmentTypeQuery, useGetStorageSiteQuery } from "./app/apiSlice.js";
 import './App.css';
 
 const EquipmentList = () => {
-    const { data: equipmentList } = useGetEquipmentQuery();
+    const { data: equipmentList, isLoading: equipmentLoading } = useGetEquipmentQuery();
+    const { data: equipmentTypes, isLoading: equipmentTypesLoading } = useGetEquipmentTypeQuery();
+    const { data: storageSites, isLoading: storageSitesLoading } = useGetStorageSiteQuery();
     const [searchQuery, setSearchQuery] = useState('');
+    console.log(equipmentTypes)
 
-    const filteredEquipment = equipmentList.filter((equipment) =>
+    if (equipmentLoading || equipmentTypesLoading || storageSitesLoading) {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <p>Loading...</p>
+                </header>
+            </div>
+        )
+    }
+
+    const filteredEquipment = equipmentList?.filter((equipment) =>
         equipment.model_name.toLowerCase().includes(searchQuery.toLowerCase())
-    );
-    console.log(equipmentList)
+    ) || [];
+
     return (
         <div className="w-screen p-4">
             <h1 className="text-2xl font-bold mb-4">Equipment List</h1>
@@ -20,7 +33,7 @@ const EquipmentList = () => {
                 onChange={(e) => setSearchQuery(e.target.value)}
                 className="border p-2 mb-4"
             />
-            <table className="min-w-full bg-white border border-gray-300 shadow-md rounded-md">
+            <table className="table-auto bg-white border border-gray-300 shadow-md rounded-md">
                 <thead>
                     <tr>
                         <th className="py-2 px-4 border-b">Model Name</th>
@@ -28,7 +41,7 @@ const EquipmentList = () => {
                         <th className="py-2 px-4 border-b">Serial Number</th>
                         <th className="py-2 px-4 border-b">Storage Site ID</th>
                         <th className="py-2 px-4 border-b">Date Serviced</th>
-                        <th className="py-2 px-4 border-b">Equipment Type Name</th>
+                        <th className="py-2 px-4 border-b">Equipment Type</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -37,9 +50,13 @@ const EquipmentList = () => {
                             <td className="py-2 px-4 border-b">{equipment.model_name}</td>
                             <td className="py-2 px-4 border-b">{equipment.description}</td>
                             <td className="py-2 px-4 border-b">{equipment.serial_number}</td>
-                            <td className="py-2 px-4 border-b">{equipment.storage_site_id}</td>
+                            <td className="py-2 px-4 border-b">
+                                {storageSites?.find(site => site.id === equipment.storage_site_id).location_address}
+                            </td>
                             <td className="py-2 px-4 border-b">{equipment.date_serviced}</td>
-                            <td className="py-2 px-4 border-b">{equipment.equipment_type.name}</td>
+                            <td className="py-2 px-4 border-b">
+                                {equipmentTypes?.find(type => type.id === equipment.equipment_type_id).name}
+                            </td>
                         </tr>
                     ))}
                 </tbody>
